1369575fac7231c71aff10c7ef2cd78877cc8977
nadym baba <nadym.baba@gmail.com>
2026-06-01 21:31:08 +0530

    feat(admin): add CategoryViewModel + HomeViewModel to AdminApp

--- android-admin/app/src/main/java/com/khanqah/admin/AdminApp.kt
+++ android-admin/app/src/main/java/com/khanqah/admin/AdminApp.kt
@@ -6,7 +6,9 @@ import com.khanqah.admin.data.api.TokenManager
 import com.khanqah.admin.data.repository.*
 import com.khanqah.admin.ui.auth.AuthViewModel
 import com.khanqah.admin.ui.bugs.BugsViewModel
+import com.khanqah.admin.ui.categories.CategoryViewModel
 import com.khanqah.admin.ui.content.ContentViewModel
+import com.khanqah.admin.ui.home.HomeViewModel
 import com.khanqah.admin.ui.live.LiveViewModel
 import com.khanqah.admin.ui.schedule.ScheduleViewModel
 import com.khanqah.admin.ui.team.TeamViewModel
@@ -21,20 +23,27 @@ class AdminApp : Application() {
     lateinit var liveViewModel: LiveViewModel
     lateinit var teamViewModel: TeamViewModel
     lateinit var bugsViewModel: BugsViewModel
+    lateinit var categoryViewModel: CategoryViewModel
+    lateinit var homeViewModel: HomeViewModel
 
     override fun onCreate() {
         super.onCreate()
         BroadcastForegroundService.createChannel(this)
         val tokenManager = TokenManager(this)
         val api = ApiClient(tokenManager).service
+        val contentRepo = ContentAdminRepository(api)
+        val scheduleRepo = ScheduleRepository(api)
+        val bugRepo = BugRepository(api)
 
         authRepo = AuthRepository(api, tokenManager)
         authViewModel = AuthViewModel(authRepo)
-        contentViewModel = ContentViewModel(ContentAdminRepository(api))
+        contentViewModel = ContentViewModel(contentRepo)
         uploadViewModel = UploadViewModel(UploadRepository(api, this))
-        scheduleViewModel = ScheduleViewModel(ScheduleRepository(api))
+        scheduleViewModel = ScheduleViewModel(scheduleRepo)
         liveViewModel = LiveViewModel(LiveRepository(api))
         teamViewModel = TeamViewModel(TeamRepository(api))
-        bugsViewModel = BugsViewModel(BugRepository(api))
+        bugsViewModel = BugsViewModel(bugRepo)
+        categoryViewModel = CategoryViewModel(CategoryRepository(api))
+        homeViewModel = HomeViewModel(contentRepo, scheduleRepo, bugRepo)
     }
 }
